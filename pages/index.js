f70bae9bb0773513d414d0ec0abe85b2ea41c72b
Sebastian Alonso Gomez Arias <s.gomez@CO-IT01991.local>
2019-01-18 07:53:08 -0500

    feat(*) Initial structure and architecture using next and React

---
+++ pages/index.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import Router from 'next/router'
+
+import Modal from '../components/modal'
+
+export default class extends React.Component {
+  static getInitialProps () {
+    return {
+      photos: new Array(15).fill(0).map((v, k) => k + 1)
+    }
+  }
+
+  constructor (props) {
+    super(props)
+    this.onKeyDown = this.onKeyDown.bind(this)
+  }
+
+  // handling escape close
+  componentDidMount () {
+    document.addEventListener('keydown', this.onKeyDown)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.onKeyDown)
+  }
+
+  onKeyDown (e) {
+    if (!this.props.url.query.photoId) return
+    if (e.keyCode === 27) {
+      this.props.url.back()
+    }
+  }
+
+  dismissModal () {
+    Router.push('/')
+  }
+
+  showPhoto (e, id) {
+    e.preventDefault()
+    Router.push(`/?photoId=${id}`, `/photo?id=${id}`)
+  }
+
+  render () {
+    const { url, photos } = this.props
+
+    return (
+      <div className='list'>
+        {
+          url.query.photoId &&
+            <Modal
+              id={url.query.photoId}
+              onDismiss={() => this.dismissModal()}
+            />
+        }
+        {
+          photos.map((id) => (
+            <div key={id} className='photo'>
+              <a
+                className='photoLink'
+                href={`/photo?id=${id}`}
+                onClick={(e) => this.showPhoto(e, id)}
+              >
+                {id}
+              </a>
+            </div>
+          ))
+        }
+        <style jsx>{`
+          .list {
+            padding: 50px;
+            text-align: center;
+          }
+
+          .photo {
+            display: inline-block;
+          }
+
+          .photoLink {
+            color: #333;
+            verticalAlign: middle;
+            cursor: pointer;
+            background: #eee;
+            display: inline-block;
+            width: 250px;
+            height: 250px;
+            line-height: 250px;
+            margin: 10px;
+            border: 2px solid transparent;
+          }
+
+          .photoLink:hover {
+            borderColor: blue;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
