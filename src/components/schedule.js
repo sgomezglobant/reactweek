f61e43fdbaf2e0b170b86c08b4c68b9cd2142db5
Andres Bedoya <mortiis.angel@gmail.com>
2019-01-18 11:56:54 -0500

    react week using gatsbyjs

---
+++ src/components/schedule.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Schedule = () => (
+  <footer>
+    <h3>Agenda</h3>
+    <p>Seccion Agenda</p>
+  </footer>
+);
+
+export default Schedule;
---
+++ src/components/volunteers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Volunteers = () => (
+  <footer>
+    <h3>React Heroes</h3>
+    <p>Seccion Voluntarios</p>
+  </footer>
+);
+
+export default Volunteers;
