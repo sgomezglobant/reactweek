0039f1c0b7a05997956ee62f9ed1fe7dc8dd779f
Angela Ordonez <angelitaooo@me.com>
2019-01-21 11:11:21 -0500

    Schedule component

--- src/components/agendaCard.js
+++ src/components/agendaCard.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import './agenda.scss'
+import "./agenda.scss";
 
-const AgendaCard = () => (
- <div className="card">
+const AgendaCard = ({ image, schedule }) => (
+  <div className="card">
     <div className="card-wrapper">
-        <img className="card-image" src="http://i.pravatar.cc/300" alt="avatar"/>
-        <div className="card-information">
-            <h2 className="schedule">Martes- 05:30pm - 6:30pm</h2>
-            <p className="card-title"><i>Arquitectura de Apps escalables en Angular</i></p>
-            <p className="card-description">Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.</p>
-            <a href="#">Slides</a>
-            <a href="#">Video</a>
-        </div>
+      <img className="card-image" src={image} alt="avatar" />
+      <div className="card-information">
+        <h2 className="schedule">{schedule}</h2>
+        <p className="card-title">
+          <i>Arquitectura de Apps escalables en Angular</i>
+        </p>
+        <p className="card-description">
+          Aprenderemos como aplicar los patrones de diseño con los bloques de
+          construcción de Angular para proyectos pensados en tener mas de 100
+          componentes.
+        </p>
+        <a href="#">Slides</a>
+        <a href="#">Video</a>
+      </div>
     </div>
   </div>
-)
+);
 
-
-
-export default AgendaCard
+export default AgendaCard;
