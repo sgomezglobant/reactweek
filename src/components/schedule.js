fbad9ad9570841ed1586accae4b3ba70ae8333b2
Jhonatan Uribe <yosoyuribe@gmail.com>
2019-01-21 12:50:29 -0500

    agenda card props

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,44 +1,23 @@
-import React from "react";
-import AgendaCard from "./agendaCard";
-import "./agenda.scss";
-
-const agendaList = [
-  {
-    image: "http://i.pravatar.cc/302",
-    schedule: "Martes- 05:30pm - 6:30pm",
-    title: "Arquitectura de Apps escalables en Angular",
-    description:
-      "Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.",
-    slides: "",
-    video: ""
-  },
-  {
-    image: "http://i.pravatar.cc/300",
-    schedule: "Miercoles- 06:30pm - 7:30pm",
-    title: "Angular Material",
-    description:
-      "Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.",
-    slides: "",
-    video: ""
-  },
-  {
-    image: "http://i.pravatar.cc/301",
-    schedule: "Martes- 08:30pm - 9:30pm",
-    title: "De Reactive Forms y Otras Delicias",
-    description:
-      "Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.",
-    slides: "",
-    video: ""
-  }
-];
+import React from 'react';
+import AgendaCard from './agendaCard';
+import data from '../data';
+import '../styles/schedule.scss';
 
 const Schedule = () => (
   <section className="agenda">
     <h2>Agenda</h2>
     <div className="agenda-grid">
-      {agendaList.map(item => (
+      {data.talks.map(item => (
         <div className="agenda-container">
-          <AgendaCard image={item.image} schedule={item.schedule} />
+          <AgendaCard
+            image={item.image}
+            schedule={item.schedule}
+            title={item.title}
+            description={item.description}
+            video={item.video}
+            slides={item.slides}
+            repository={item.repository}
+          />
         </div>
       ))}
     </div>
