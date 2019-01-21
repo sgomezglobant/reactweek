039e17d8e1f81c5342d4ddb52cd6320c116857ea
Angela Ordonez <angelitaooo@me.com>
2019-01-21 09:37:26 -0500

    add styles to agenda card

--- src/components/agendaCard.js
+++ src/components/agendaCard.js
@@ -7,9 +7,9 @@ const AgendaCard = () => (
     <div className="card-wrapper">
         <img className="card-image" src="http://i.pravatar.cc/300" alt="avatar"/>
         <div className="card-information">
-            <h3 className="schedule">Martes- 05:30pm - 6:30pm</h3>
-            <p className="card-title">Arquitectura de Apps escalables en Angular</p>
-            <p>Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.</p>
+            <h2 className="schedule">Martes- 05:30pm - 6:30pm</h2>
+            <p className="card-title"><i>Arquitectura de Apps escalables en Angular</i></p>
+            <p className="card-description">Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.</p>
             <a href="#">Slides</a>
             <a href="#">Video</a>
         </div>
