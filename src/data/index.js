1f8b4a216cb6323f4cf9bb7b4c39323b3b87422b
Jose Daniel Vasquez Garcia <josedaniel.vasquez@globant.com>
2019-01-23 13:22:37 -0500

    Volunteers component was created

--- src/data/index.js
+++ src/data/index.js
@@ -67,5 +67,47 @@ export default {
         url: 'http://pionerasdev.co/'
       },
     ]
+  },
+  volunteers: {
+    volunteersTitle: 'Heroes Reactweek',
+    volunteersList: [
+      {
+        image: 'Angela Ordoñez',
+        name: 'Angela ordoñez',
+        url: 'https://avatars3.githubusercontent.com/u/18565471?v=4'
+
+      },
+      {
+        image: 'Sebastian Gomez',
+        name: 'Sebastian Gomez',
+        url: 'https://avatars1.githubusercontent.com/u/20705054?v=4'
+
+      },
+      {
+        image: 'Jhonatan Uribe',
+        name: 'Jhonatan Uribe',
+        url: 'https://avatars1.githubusercontent.com/u/11787339?v=4'
+
+      },
+      {
+        image: 'Jorge Ivan Morales',
+        name: 'Jorge Ivan Morales',
+        url: 'https://avatars3.githubusercontent.com/u/1750884?v=4'
+
+      },
+      {
+        image: 'Omar',
+        name: 'Omar',
+        url: 'https://avatars1.githubusercontent.com/u/8124929?v=4'
+
+      },
+      {
+        image: 'Andres',
+        name: 'Andres',
+        url: 'https://avatars0.githubusercontent.com/u/315504?v=4'
+
+      }
+
+    ]
   }
 };
