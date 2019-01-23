af85fd26fee71c4f88bb03d11794a2d050350f9c
Juan Mateo Garcia Ramirez <juanmateo.garcia@globant.com>
2019-01-23 08:52:54 -0500

    Write base jsx

---
+++ src/components/register.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "registrer.scss";
+
+
+/* Register Eventbrite widget */
+
+const Register = () => (
+  <section className="register">
+    <header className="register-header">
+      <h2 className="register-header__title">Speakers</h2>
+    </header>
+    <main className="register-widget">
+    </main>
+  </section>
+)
+
+export default Register;
