1e8a04f6a8527c0d68cf0794867808ef7957d41c
Juan Mateo Garcia Ramirez <juanmateo.garcia@globant.com>
2019-01-23 11:15:51 -0500

    Change speakers struct

--- src/components/speakers.js
+++ src/components/speakers.js
@@ -7,8 +7,8 @@ import "./../styles/speakers.scss";
 
 const Speakers = () => (
   <section className="speakers">
-    <header className="speakers-details">
-      <h2 className="speakers-details__title">Speakers</h2>
+    <header className="speakers__details">
+      <h2 className="speakers__title">Speakers</h2>
     </header>
     <main className="speakers-grid">
       {resources.speakers.map(({id, ...speaker}) => <SpeakerItem key={id} {...speaker} />)}
