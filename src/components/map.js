699349f9b606d07492e859b42158fafc3292ffc8
Duvan G <duvan258@gmail.com>
2019-01-23 10:34:33 -0500

    Add openStreetMap in map section

--- src/components/map.js
+++ src/components/map.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Map = () => (
   <footer>
-    <h3>Mapa</h3>
-    <p>Seccion Mapa</p>
+    <h3>Ubicación</h3>
+    <p>Centro empresarial vizcaya...</p>
+    <iframe
+      width="100%"
+      height="350"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=-75.56533813476564%2C6.207023769201901%2C-75.56179761886598%2C6.2094822627699555&amp;layer=mapnik"
+    />
   </footer>
 );
 
