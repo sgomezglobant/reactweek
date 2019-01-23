97f3bb2b7ee1ea26c2f181c826b930b7ada97656
Juan Mateo Garcia Ramirez <juanmateo.garcia@globant.com>
2019-01-23 11:02:57 -0500

    Change speakers data origin

--- src/data/index.js
+++ src/data/index.js
@@ -39,5 +39,88 @@ export default {
       repository: 'https://github.com/CarlosAngulo/AnimationBuilder-basics',
       video: 'https://youtu.be/mPMIFaVHLlY?t=1h16m23s'
     }
+  ],
+  speakers : [
+    {
+      id: 1,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 2,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 3,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 4,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 5,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 6,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 7,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 8,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    },
+    {
+      id: 9,
+      pic: "https://dummyimage.com/256x256/000/fff",
+      name: "Juan Vasquez",
+      description: "Soy Juanfe Vásquez, padre de familia (1 hijo y 4 perros) y fanático de libros de ciencia ficción. En mi carrera profesional me he desempeñado como UX Designer, Profesor, Líder de Área y Front-End Developer.",
+      github: "https://github.com/sgomezglobant/reactweek",
+      facebook: "https://github.com/sgomezglobant/reactweek",
+      twitter: "https://github.com/sgomezglobant/reactweek"
+    }
   ]
 };
