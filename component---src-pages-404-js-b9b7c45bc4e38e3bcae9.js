2cb5779a82fc4088429344bb516686e94090daf8
Sebastian Alonso Gomez Arias <s.gomez@CO-IT01991.local>
2019-01-25 10:36:49 -0500

    Updates

---
+++ component---src-pages-404-js-b9b7c45bc4e38e3bcae9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{137:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(150),o=n(147);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b9b7c45bc4e38e3bcae9.js.map
