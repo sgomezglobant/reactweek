3abb2df3a55ea02685d0313f4933060b1096abf8
Sebastian Alonso Gomez Arias <s.gomez@CO-IT01991.local>
2019-01-25 10:50:08 -0500

    Adding React Medellin

--- src/data/index.js
+++ src/data/index.js
@@ -40,47 +40,6 @@ export default {
             video: 'https://youtu.be/mPMIFaVHLlY?t=1h16m23s'
         }
     ],
-    talks: [
-        {
-            schedule: 'Martes 05:30pm - 6:30pm',
-            image: 'http://i.pravatar.cc/300',
-            title: 'Arquitectura de Apps escalables en Angular',
-            description:
-                'Aprenderemos como aplicar los patrones de diseño con los bloques de construcción de Angular para proyectos pensados en tener mas de 100 componentes.',
-            slides: 'https://slides.com/carlosrojas_o/angular-week-2018#/',
-            video: 'https://youtu.be/62EtV33NWmc?t=25m25s'
-        },
-        {
-            schedule: 'Martes 06:30pm - 7:30pm',
-            image: 'http://i.pravatar.cc/302',
-            title: 'Angular Material',
-            description:
-                'Aprenderemos el uso del CDK para crear componentes increíbles y de alta calidad, usando las características de Angular Material.',
-            slides: 'https://slides.com/vanessamarely/angular-week-2018-3#/',
-            video: 'https://youtu.be/62EtV33NWmc?t=45m45s'
-        },
-        {
-            image: 'http://i.pravatar.cc/301',
-            schedule: 'Miércoles 05:30pm - 06:30pm',
-            title: 'De Reactive Forms y Otras Delicias',
-            description:
-                'Aprende a crear formularios dinámicos y profundamente customizados, además de técnicas avanzadas de validación y optimización de manipulación de datos.',
-            slides:
-                'https://slides.com/juantobongt/reactive-forms-y-otras-delicias#/',
-            repository: 'https://github.com/juantobongt/ng-form-examples',
-            video: 'https://youtu.be/mPMIFaVHLlY?t=11m24s'
-        },
-        {
-            image: 'http://i.pravatar.cc/302',
-            schedule: 'Miércoles- 06:30 pm - 7:30pm',
-            title: "Let's build your Angular Animations",
-            description:
-                'Ya que una animación vale más que mil imágenes, aprendamos a transformar tus soluciones en atractivas aplicaciones dinámicas utilizando Angular AnimationBuilder. Comenzando desde cero.',
-            slides: 'https://slides.com/carlosangulo/angular-week-2018',
-            repository: 'https://github.com/CarlosAngulo/AnimationBuilder-basics',
-            video: 'https://youtu.be/mPMIFaVHLlY?t=1h16m23s'
-        }
-    ],
     speakers: [
         {
             id: 1,
@@ -176,9 +135,9 @@ export default {
         ],
         communities: [
             {
-                image: 'angular_medellin.png',
-                name: 'Angular Medellin',
-                url: 'https://github.com/angular-medellin/ngcolombia-2017'
+                image: 'react-medellin.png',
+                name: 'React Medellín',
+                url: 'https://www.meetup.com/es-ES/React-Medellin/events/258336352/'
             },
             {
                 image: 'gdg-logo2.png',
